a5b84c5d0add518e8b33ee8db6f38a79bf8bd858
lifishake <lifishake@gmail.com>
2017-02-07 16:30:19 +0800

    single忘传了.

---
+++ template-parts/post/content-single.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Template part for displaying posts
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package WordPress
+ * @subpackage SkyWarp2
+ * @since 1.0
+ * @version 1.0
+ */
+
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<?php
+		if ( is_sticky() && is_home() ) :
+			echo sw2_get_svg( array( 'icon' => 'thumb-tack' ) );
+		endif;
+	?>
+	<header class="entry-header">
+		<?php
+			if ( 'post' === get_post_type() ) :
+				echo '<div class="entry-meta">';
+						echo skywarp2_time_link();
+						sw2_edit_link();
+				echo '</div><!-- .entry-meta -->';
+			endif;
+
+			if ( is_single() ) {
+				the_title( '<h1 class="entry-title">', '</h1>' );
+			} else {
+				the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+			}
+		?>
+	</header><!-- .entry-header -->
+
+	<div class="entry-content">
+		<?php
+			/* translators: %s: Name of current post */
+            if ( is_single() ) {
+                the_content();
+            }
+            else {
+                the_excerpt();
+            }
+
+		?>
+	</div><!-- .entry-content -->
+
+	<?php if ( is_single() ) : ?>
+		<?php twentyseventeen_entry_footer(); ?>
+	<?php endif; ?>
+
+</article><!-- #post-## -->
